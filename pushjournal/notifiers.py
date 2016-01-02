3377d25287f7b77e049646a3122f92b8f98c3699
Roey Darwish Dror <roey.ghost@gmail.com>
2016-01-02 15:58:13 +0200

    Add SMTP notifier

--- pushjournal/notifiers.py
+++ pushjournal/notifiers.py
@@ -1,4 +1,5 @@
 import socket
+from smtplib import SMTP
 import pushbullet
 from . import config
 
@@ -20,6 +21,31 @@ class Pushbullet(Notifier):
         self._pb.push_note(title, message)
 
 
+class Smtp(Notifier):
+    def __init__(self, smtp_host, smtp_port, username, password, use_tls, from_addr, to_addrs):
+        self._smtp_host = smtp_host
+        self._smtp_port = smtp_port
+        self._username = username
+        self._password = password
+        self._use_tls = use_tls
+        self._from_addr = from_addr
+        self._to_addrs = to_addrs
+
+    def notify(self, title, message):
+        mailserver = SMTP(self._smtp_host, self._smtp_port)
+        if self._use_tls:
+            mailserver.starttls()
+
+        if self._username:
+            mailserver.login(self._username, self._password)
+
+        message = "From: {from_addr}\nTo:{to}\nSubject:{title}\n{message}".format(
+            from_addr=self._from_addr, to=", ".join(self._to_addrs), title=title, message=message)
+
+        mailserver.sendmail(self._from_addr, self._to_addrs, message)
+        mailserver.quit()
+
+
 def create_notifiers(app_config):
     notifiers = []
     for n in app_config['notifiers']:
@@ -29,6 +55,19 @@ def create_notifiers(app_config):
             if "key" not in n:
                 raise config.ConfigError("Missing key for Pushbullet notifier")
             notifiers.append(Pushbullet(n['key'], n.get('prepend_hostname', False)))
-
+        if n["type"] == "smtp":
+            for required in ["host", "from", "to"]:
+                if required not in n:
+                    raise config.ConfigError("\"{}\" is a required value for SMTP notifier")
+            notifiers.append(Smtp(
+                n['host'],
+                int(n.get('port', 25)),
+                n.get("user"),
+                n.get("password"),
+                n.get("use_tls", False),
+                n['from'],
+                n['to']))
+        else:
+            raise config.ConfigError("Unknown notifer type {}".format(n['type']))
 
     return notifiers
